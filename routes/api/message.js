60f4deb816c0e219e249c6c10399fa08784a9092
hohohoho1223 <dnjsgh1820@naver.com>
2025-07-24 14:31:01 +0900

    - DELETE 라우터 추가: DELETE /:messageId - 인증 미들웨어 적용

--- routes/api/message.js
+++ routes/api/message.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const messageController = require('../controllers/messageController');
-const auth = require('../middleware/auth');
+const messageController = require('../../controllers/messageController');
+const auth = require('../../middleware/auth');
 
 // 채팅방의 메시지 목록 조회
 router.get('/rooms/:roomId/messages', auth, messageController.loadMessages);
+
+// 메시지 삭제
+router.delete('/:messageId', auth, messageController.deleteMessage);
 
 module.exports = router;
